ec556acc061280aa658315f16074227da53dd2fb
Shubham_yadav <shubhamyadav.456ss@gmail.com>
2024-06-13 23:02:17 +0530

    "changes made for bcrypt"

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname,'./client/build/index.html'))
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 1000;
 //run listen
 app.listen(PORT, () => {
   console.log(
